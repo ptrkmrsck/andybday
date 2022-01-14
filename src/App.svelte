4a5708ca9910c7fe6e163eeb889e4b9f20cba0aa
Patrick Marschke <patrick.m.marschke@gmail.com>
2022-01-13 22:09:52 -0600

    initial

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Pix from "./pix.svelte"
+</script>
+
+<main>
+	<h1>hbd champion!</h1>
+	<div class=container>
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+		<Pix />
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		margin: 0;
+	}
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+		margin: 0;
+	}
+	.container {
+		display: grid;
+		grid-template-columns:  1fr 1fr 1fr;
+		border: 1px solid red;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/pix.svelte
@@ -0,0 +1,15 @@
+<script>
+  let picNum = Math.floor(Math.random() * 4) + 1
+
+  let src = '/andrew1.png';
+  let picTempo = setInterval(() => {
+    src = `/andrew${Math.floor(Math.random() * 4) + 1}.png`
+  }, 500);
+</script>
+
+<img {src} alt="andy flexin">
+
+
+<style>
+
+</style>
